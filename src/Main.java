c3beeefc6bf7a195547b2206ce8bab15f59567fd
Denis Kohan <deniskochan01@gmail.com>
2024-09-26 02:52:09 +0300

    доделаная практическая номер 4 и доп задания

--- src/Main.java
+++ src/Main.java
@@ -128,3 +128,108 @@ public class RandomSeries {
     }
 }
 */
+
+
+/*
+Доп задания по циклам и условиям*/
+
+/*Задание 1*/
+
+
+/*
+import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1;i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}*/
+
+/*Задание 2*/
+
+/*import java.io.Console;
+
+public class Main {
+    public static void main(String[] args) {
+int sum = 0;
+        for (int i = 1;i <= 100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}*/
+
+
+/*
+Задание 3*/
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        int sum = 1;
+        for (int i = 1;i <= 5; i++){
+            sum *= i;
+        }
+        System.out.println(sum);
+    }
+}
+*/
+
+/*Задание 4*/
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+
+            System.out.println();
+    }
+}
+}*/
+
+
+/*
+Задание 5*/
+
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+
+        if (number % 2 == 0){
+            System.out.println("Четные числа от 1 до " + number + ": ");
+            for (int i = 1; i <= number; i++){
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+
+            }
+        }
+
+        else {
+            System.out.println("Нечетные числа от 1 до " + number + ":");
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 != 0) {
+                    System.out.println(i);
+                }
+        }
+
+    }
+}
+}*/
